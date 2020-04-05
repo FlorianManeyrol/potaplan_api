c227f5465d4aa3e451faca359d7811752f76bcfb
florianmaneyrol <florianmaneyrol@gmail.com>
2020-04-05 21:57:27 +0200

    added delete method to plants controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -9,6 +9,12 @@ class PlantsController < ApplicationController
     render json: @plant, status: :created
   end
 
+  def delete
+    @plant = Plant.find(params[:id])
+    @plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/plants", to: 'plants#index'
   post "/plants", to: 'plants#create'
+  delete "/plants/:id", to: 'plants#delete'
 end
